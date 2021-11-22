5d9c99a91f38c221603fab5b0a9460e4f2104170
DalilaC33 <dalila.castelnovo@fiuni.edu.py>
2021-11-22 10:14:46 -0300

    Update CasoDerivadoB.java

--- src/main/java/com/sd/clientsd/beans/CasosDerivados/CasoDerivadoB.java
+++ src/main/java/com/sd/clientsd/beans/CasosDerivados/CasoDerivadoB.java
@@ -16,6 +16,50 @@ public class CasoDerivadoB extends BaseBean {
 
     @Override
     protected void create(Map<String, String> params) {
+        if(params.containsKey("id") && params.get("id")!=null){
+            setId(Integer.valueOf(params.get("id")));
+        }else{
+            setId(0);
+        }
+        /*
+        setDate(params.get("date"));
+        setDescription(params.get("description"));
+        setTrabajador_social_id(params.get("trabajador_social_id"));
+        */
 
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getTrabajador_social_id() {
+        return trabajador_social_id;
+    }
+
+    public void setTrabajador_social_id(Integer trabajador_social_id) {
+        this.trabajador_social_id = trabajador_social_id;
+    }
+
+    @Override
+    public Integer getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(Integer id) {
+        super.setId(id);
     }
 }
